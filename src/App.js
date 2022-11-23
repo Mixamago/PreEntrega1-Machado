d7622be21885b9777b141c09ea0181fd247f96ed
Mixamago <mixamago@gmail.com>
2022-11-23 19:13:18 -0300

    Probando el commit.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esto es para probar lo que estoy haciendo.
         </p>
         <a
           className="App-link"
